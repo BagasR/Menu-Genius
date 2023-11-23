c41237302f984509ace3847758b01664deeb0ff6
Bagas kara <bagaskaradwirizky7@gmail.com>
2023-11-23 19:49:30 +0700

    update final

--- app.py
+++ app.py
@@ -1,30 +1,19 @@
-from flask import Flask,render_template,request,jsonify
+from flask import Flask, render_template, request, jsonify
 import pandas as pd
-import numpy as np
 from sklearn.tree import DecisionTreeClassifier
 from joblib import load
 
-app   = Flask(__name__, static_url_path='/static')
+app = Flask(__name__, static_url_path='/static')
 model = None
 
-# Baca data nama restoran dari file CSV
+# Read data nama restoran from CSV
 nama_restoran_df = pd.read_csv('nama_restoran.csv')
 
-# Dictionary pemetaan 
-peta_kecamatan = {
-    "1": "Syiah Kuala",
-    "2": "Kuta Alam",
-    "3": "Banda Raya",
-    "4": "Meuraxa"
-}
-peta_tempat = {
-    "1": "Restoran",
-    "2": "Cafe"
-}
-peta_kabupaten = {
-    "1": "Banda Aceh",
-    "2": "Aceh Besar"
-}
+# Mapping Dictionaries untuk kecamatan, jenis restoran, kabupaten, dan rating
+peta_kecamatan = {"1": "Syiah Kuala", "2": "Kuta Alam", "3": "Banda Raya", "4": "Meuraxa"}
+peta_cr_angka = {"1": "restoran", "2": "cafe"}
+peta_kabupaten = {"1": "Banda Aceh", "2": "Aceh Besar"}
+peta_reting = {"1.0": "1", "2.0": "2", "3.0": "3", "4.0": "4", "5.0": "5"}
 
 # Membuat kamus yang berisi nama restoran, alamat, dan hasil prediksi
 nama_restoran_peta = {}
@@ -35,36 +24,36 @@ for index, row in nama_restoran_df.iterrows():
     alamat = row['alamat']
     kecamatan = row['kecamatan']
     kabupaten = row['kabupaten']
+    crangka = row['crangka']
     harga = row['harga']
-    nama_restoran_peta[hasil_prediksi] = {'nama_restoran': nama_restoran, 'menu_makanan': menu_makanan, 'kecamatan': kecamatan, 'kabupaten': kabupaten, 'alamat': alamat, 'harga': harga}
+    reting = row['reting']
+    nama_restoran_peta[hasil_prediksi] = {
+        'nama_restoran': nama_restoran,
+        'menu_makanan': menu_makanan,
+        'kecamatan': kecamatan,
+        'kabupaten': kabupaten,
+        'crangka': crangka,
+        'alamat': alamat,
+        'harga': harga,
+        'reting': reting
+    }
 
+# Routes
 @app.route("/")
 def beranda():
     return render_template('index.html')
 
+#API deteksi
 @app.route("/api/deteksi", methods=['POST'])
 def apiDeteksi():
     if request.method == 'POST':
-        # Set nilai untuk variabel input atau features (X) berdasarkan input dari pengguna
-        input_harga_rata = str(request.form['Harga_Rata-rata'])
+        input_harga_rata = float(request.form['Harga_Rata-rata'])
         input_rating = float(request.form['rating'])
         input_kecamatan = str(request.form['kecamatan'])
         input_kabupaten = str(request.form['kabupaten'])
         input_cr_angka = str(request.form['crangka'])
 
-        # Nilai default untuk variabel input atau features (X) ke model
-        if input_harga_rata == "Harga_Rata-rata":
-            input_harga_rata = "15000"
-        if input_rating == "rating":
-            input_rating = 4.6
-        if input_kecamatan == "kecamatan":
-            input_kecamatan = "2"
-        if input_kabupaten == "kabupaten":
-            input_kabupaten = "2"
-        if input_cr_angka == "crangka":
-            input_cr_angka = "2"
-
-        # Load data ke dalam DataFrame
+        # Membuat DataFrame dari input
         df = pd.DataFrame(data={
             "Harga_Rata": [input_harga_rata],
             "rating": [input_rating],
@@ -73,63 +62,100 @@ def apiDeteksi():
             "CR_Angka": [input_cr_angka]
         })
 
+        # Melakukan prediksi dengan model machine learning
         hasil_prediksi = model.predict(df[0:1])[0]
 
-        # Filter hasil prediksi sesuai dengan kondisi tertentu
-        # Misalnya, hanya restoran dengan hasil prediksi >= 0.5 yang diberikan
+
+        # menetapkan suatu batas atau ambang nilai
         threshold = 0.5
         if hasil_prediksi < threshold:
             return jsonify({
                 "prediksi": "Tidak memenuhi threshold",
                 "info_kecamatan": peta_kecamatan.get(input_kecamatan, {}),
                 "info_kabupaten": peta_kabupaten.get(input_kabupaten, {}),
+                "info_crangka": peta_cr_angka.get(input_cr_angka, {}),
+                "info_reting": peta_cr_angka.get(input_rating, {}),
                 "rekomendasi_restoran": []
             })
 
-        # Cari nama restoran dan alamat sesuai dengan hasil prediksi dari peta
-        restoran_info = nama_restoran_peta.get(hasil_prediksi, {})
-        nama_restoran = restoran_info.get('nama_restoran', 'Restoran tidak ditemukan')
-        menu_makanan = restoran_info.get('menu_makanan', 'menu makananan tidak ditemukan')
-        alamat = restoran_info.get('alamat', 'Alamat tidak ditemukan')
-        harga = restoran_info.get('harga', 'Harga tidak ditemukan')
 
-        # Dapatkan nilai harga yang diinputkan oleh pengguna
+        # membaca nilai input rating
+        print(f'Nilai input_rating: {input_rating}')
+
+         # Mengambil nilai input lainnya dari peta
         input_harga = float(input_harga_rata)
-
         input_kecamatan = peta_kecamatan.get(input_kecamatan)
         input_kabupaten = peta_kabupaten.get(input_kabupaten)
-        # Filter data restoran berdasarkan rentang harga
-        # Filter data restoran berdasarkan rentang harga dan kecamatan
-        filtered_restaurants = nama_restoran_df[(nama_restoran_df['harga'] <= input_harga + 10000) &
-                                        (nama_restoran_df['harga'] >= input_harga - 10000) &
-                                        (nama_restoran_df['kecamatan'] == input_kecamatan) & (nama_restoran_df['kabupaten'] == input_kabupaten)]
+        input_cr_angka = peta_cr_angka.get(input_cr_angka)
 
+        # Memfilter dataframe data CSV nama_restoran
+        filtered_restaurants = nama_restoran_df[
+            (nama_restoran_df['harga'] <= input_harga + 10000) &
+            (nama_restoran_df['harga'] >= input_harga - 10000) &
+            (nama_restoran_df['reting'] == input_rating) &
+            (nama_restoran_df['kecamatan'] == input_kecamatan) & 
+            (nama_restoran_df['kabupaten'] == input_kabupaten) & 
+            (nama_restoran_df['crangka'] == input_cr_angka)
+        ]
 
-        # Ambil 6 restoran terdekat dari data yang sudah difilter
+        # inisialisasi list  untuk menyimpan rekomendasi restoran
         rekomendasi_restoran = []
+        # Iterasi Melalui Hasil Filter
         for index, row in filtered_restaurants.iterrows():
             if row['hasil_prediksi'] != hasil_prediksi:
-                rekomendasi_restoran.append({'nama_restoran': row['nama_restoran'], 'menu_makanan': row['menu_makanan'], 'alamat': row['alamat'], 'kecamatan': row['kecamatan'], 'kabupaten': row['kabupaten'], 'harga': row['harga']})
+                rekomendasi_restoran.append({
+                    'nama_restoran': row['nama_restoran'],
+                    'menu_makanan': row['menu_makanan'],
+                    'alamat': row['alamat'],
+                    'kecamatan': row['kecamatan'],
+                    'kabupaten': row['kabupaten'],
+                    'crangka': row['crangka'],
+                    'reting': row['reting'],
+                    'harga': row['harga']
+                })
+                # Batasi jumlah rekomendasi hingga 20 restoran
                 if len(rekomendasi_restoran) == 20:
                     break
 
-            rekomendasi_list = [{'nama_restoran': r['nama_restoran'], 'menu_makanan': r['menu_makanan'], 'alamat': r['alamat'], 'kecamatan': r['kecamatan'], 'kabuapten': r['kabupaten'], 'harga': r['harga']} for r in rekomendasi_restoran]
+        # Jika tidak ada rekomendasi restoran yang ditemukan
+        if not rekomendasi_restoran:
+            return jsonify({
+                "prediksi": "Tidak memenuhi threshold",
+                "info_kecamatan": peta_kecamatan.get(input_kecamatan, {}),
+                "info_kabupaten": peta_kabupaten.get(input_kabupaten, {}),
+                "info_crangka": peta_cr_angka.get(input_cr_angka, {}),
+                "info_reting": peta_reting.get(input_rating, {}),
+                "rekomendasi_restoran": []
+            })
+        
+        # Mengonversi list rekomendasi menjadi format yang sesuai untuk JSON
+        rekomendasi_list = [{
+            'nama_restoran': r['nama_restoran'],
+            'menu_makanan': r['menu_makanan'],
+            'alamat': r['alamat'],
+            'kecamatan': r['kecamatan'],
+            'kabupaten': r['kabupaten'],
+            'crangka': r['crangka'],
+            'reting': r['reting'],
+            'harga': r['harga']
+        } for r in rekomendasi_restoran]
 
-        # Cari informasi kecamatan sesuai dengan input_kecamatan
         info_kecamatan = peta_kecamatan.get(input_kecamatan, {})
         info_kabupaten = peta_kabupaten.get(input_kabupaten, {})
+        info_crangka = peta_cr_angka.get(input_cr_angka, {})
+        info_reting = peta_reting.get(input_rating, {})
 
-        # Print hasil prediksi, nama restoran, dan alamat restoran
         print(f'Hasil Prediksi: {hasil_prediksi}')
         print(f'Data Nama Restoran: {nama_restoran}')
         print(f'Data Menu Makanan: {menu_makanan}')
         print(f'Data Alamat Restoran: {alamat}')
         print(f'Data Harga Restoran: {harga}')
-        print(f'Info Kecamatan: {info_kecamatan}')
-        print(f'Info Kabupaten: {info_kabupaten}')
+        print(f'Info Kecamatan: {kecamatan}')
+        print(f'Info Kabupaten: {kabupaten}')
+        print(f'Info crangka: {crangka}')
         print(model)
 
-        # Return hasil prediksi dengan format JSON
+        # Mengembalikan hasil prediksi dan rekomendasi dalam format JSON
         return jsonify({
             "prediksi": str(hasil_prediksi),
             "nama_restoran": nama_restoran,
@@ -138,12 +164,11 @@ def apiDeteksi():
             "harga": harga,
             "rekomendasi_restoran": rekomendasi_list,
             "info_kecamatan": info_kecamatan,
-            "info_kabupaten": info_kabupaten
+            "info_kabupaten": info_kabupaten,
+            "info_crangka": info_crangka,
+            "info_reting": info_reting
         })
 
 if __name__ == '__main__':
-    # Load model yang telah ditraining
     model = load('resto.model')
-
-    # Run Flask di Google Colab menggunakan ngrok
-    app.run()
+    app.run(debug=True)
--- static/js/client_side.js
+++ static/js/client_side.js
@@ -1,104 +1,149 @@
- $(document).ready(function(){
-  
+$(document).ready(function() {
+
   // -[Animasi Scroll]---------------------------
-  
+
   $(".navbar a, footer a[href='#halamanku']").on('click', function(event) {
     if (this.hash !== "") {
       event.preventDefault();
       var hash = this.hash;
       $('html, body').animate({
         scrollTop: $(hash).offset().top
-      }, 900, function(){
+      }, 900, function() {
         window.location.hash = hash;
       });
-    } 
+    }
   });
-  
+
   $(window).scroll(function() {
-    $(".slideanim").each(function(){
+    $(".slideanim").each(function() {
       var pos = $(this).offset().top;
       var winTop = $(window).scrollTop();
-        if (pos < winTop + 600) {
-          $(this).addClass("slide");
-        }
+      if (pos < winTop + 600) {
+        $(this).addClass("slide");
+      }
     });
   });
 
-  
+
   // -[Prediksi Model]---------------------------
-  
+
   // Fungsi untuk memanggil API ketika tombol prediksi ditekan
   $("#prediksi_submit").click(function(e) {
-    e.preventDefault(); 
+    e.preventDefault();
 
-    var input_harga_rata = $("#rentang_harga").val(); 
+    var input_harga_rata = $("#rentang_harga").val();
     var input_rating = $("#rating").val();
     var input_kecamatan = $("#kecamatan").val();
-    var input_kabupaten = $("#kabupaten").val(); 
+    var input_kabupaten = $("#kabupaten").val();
     var input_cr_angka = $("#crangka").val();
 
-	// Panggil API dengan timeout 1 detik (1000 ms)
+    // Panggil API dengan timeout 1 detik (1000 ms)
     setTimeout(function() {
-	  try {
-			$.ajax({
-			  url  : "/api/deteksi",
-			  type : "POST",
-			  data : {
-          "Harga_Rata-rata" : input_harga_rata,
-          "rating" : input_rating,
-          "kecamatan" : input_kecamatan,
-          "kabupaten"  : input_kabupaten,
-          "crangka"  : input_cr_angka,
-        },
-			  success:function(res){
-				// Ambil hasil prediksi 
-				res_data_prediksi   = res['prediksi']
-        res_data_nama_restoran = res['nama_restoran']
-        res_data_menu_makanan = res['menu_makanan']
-        res_data_alamat = res['alamat']
-        res_data_harga = res['harga']
-        res_data_rekomendasi_restoran = res['rekomendasi_restoran']
-				// res_gambar_prediksi = res['gambar_prediksi']
+      try {
+        $.ajax({
+          url: "/api/deteksi",
+          type: "POST",
+          data: {
+            "Harga_Rata-rata": input_harga_rata,
+            "rating": input_rating,
+            "kecamatan": input_kecamatan,
+            "kabupaten": input_kabupaten,
+            "crangka": input_cr_angka
+          },
+          success: function(res) {
+            // Ambil hasil prediksi
+            res_data_prediksi = res['prediksi']
+            res_data_nama_restoran = res['nama_restoran']
+            res_data_menu_makanan = res['menu_makanan']
+            res_data_alamat = res['alamat']
+            res_data_crangka = res['crangka']
+            res_data_harga = res['harga']
+            res_data_reting = res['reting']
+            res_data_rekomendasi_restoran = res['rekomendasi_restoran']
 
-          console.log(res_data_prediksi)
-          console.log('test data rekomendasi \n', res_data_nama_restoran)
-				// Tampilkan hasil prediksi ke halaman web
-			    generate_prediksi(res_data_prediksi, res_data_nama_restoran, res_data_menu_makanan, res_data_alamat, res_data_harga, res_data_rekomendasi_restoran);
-			  }
-			});
-		}
-		catch(e) {
-			// Jika gagal memanggil API, tampilkan error di console
-			console.log("Gagal !");
-			console.log(e);
-		} 
+            console.log(res_data_prediksi)
+            console.log('test data rekomendasi \n', res_data_nama_restoran)
+            // Tampilkan hasil prediksi ke halaman web
+            generate_prediksi(res_data_prediksi, res_data_nama_restoran, res_data_menu_makanan, res_data_alamat, res_data_crangka, res_data_harga, res_data_reting, res_data_rekomendasi_restoran);
+          }
+        });
+      } catch (e) {
+        // Jika gagal memanggil API, tampilkan error di console
+        console.log("Gagal !");
+        console.log(e);
+      }
     }, 1000)
-    
+
   })
-    
+
   // Fungsi untuk menampilkan hasil prediksi model
-  function generate_prediksi(data_prediksi, data_nama_restoran, data_menu_makanan, data_alamat, data_harga, rekomendasi_restoran) {
+  function generate_prediksi(data_prediksi, data_nama_restoran, data_menu_makanan, data_alamat, data_crangka, data_reting, data_harga, rekomendasi_restoran) {
     var str_prediksi = "";
-    str_prediksi += "<h3><b>Nama restoran   : <b>" + data_nama_restoran + "</h3>";
-    str_prediksi += "<h3><b>Menu Makanan   : <b>" + data_menu_makanan + "</h3>";
-    str_prediksi += "<h3><b>Alamat restoran : <b>" + data_alamat + "</h3>";
-    str_prediksi += "<h3><b>Harga : <b>" + data_harga + "</h3>";
-    
+
     var rekomendasi_str = "";
-    for (var i = 0; i < rekomendasi_restoran.length; i++) {
-        rekomendasi_str += "<p>" + rekomendasi_restoran[i].nama_restoran + " - " + rekomendasi_restoran[i].menu_makanan + " - " + rekomendasi_restoran[i].alamat + " - " + rekomendasi_restoran[i].kecamatan + " - " + rekomendasi_restoran[i].harga + "</p>";
+    if (rekomendasi_restoran.length > 0) {
+      for (var i = 0; i < rekomendasi_restoran.length; i++) {
+        // Menghasilkan HTML untuk menampilkan rekomendasi restoran
+        rekomendasi_str += `
+          <li>
+              <div class="card-content">
+                  <h3 class="h3">
+                      <a style="color: black;" class="card-title">
+                          ${rekomendasi_restoran[i].nama_restoran}
+                      </a>
+                  </h3>
+                  <ul class="card-list">
+                      <li class="card-item">
+                          <div class="item-icon">
+                              <a style="color: black;" class="fa-solid fa-utensil-spoon">
+                              </a>
+                          </div>
+                          <span class="item-text">${rekomendasi_restoran[i].menu_makanan}</span>
+                      </li>
+                      <li class="card-item">
+                          <div class="item-icon">
+                              <a style="color: black;" class="fa-solid fa-map-marker">
+                              </a>
+                          </div>
+                          <span class="item-text">${rekomendasi_restoran[i].alamat}</span>
+                      </li>
+                      <li class="card-item">
+                          <div class "item-icon">
+                              <a style="color: black;" class="fa-solid fa-map">
+                              </a>
+                          </div>
+                          <span class="item-text">${rekomendasi_restoran[i].kecamatan}</span>
+                      </li>
+                      <li class="card-item">
+                          <div class="item-icon">
+                              <a style="color: black;" class="fa-solid fa-map">
+                              </a>
+                          </div>
+                          <span class="item-text">${rekomendasi_restoran[i].crangka}</span>
+                      </li>
+                      <li class="card-item">
+                          <div class="item-icon">
+                              <a style="color: black;" class="fa-solid fa-star">
+                              </a>
+                          </div>
+                          <span class="item-text">${rekomendasi_restoran[i].reting}</span>
+                      </li>
+                  </ul>
+                  <div class="card-meta">
+                      <div>
+                          <span class="meta-title">Harga</span>
+                          <span class="meta-text">Rp ${new Intl.NumberFormat('en-US').format(rekomendasi_restoran[i].harga)}</span>
+                      </div>
+                  </div>
+              </div>
+          </li>`;
+      }
+    } else {
+      rekomendasi_str = "Tidak ada rekomendasi tersedia.";
     }
+
+    // Menyisipkan hasil rekomendasi ke dalam elemen HTML
     var hasilRekomendasi = document.getElementById("hasil_rekomendasi");
     hasilRekomendasi.innerHTML = rekomendasi_str;
-
-    $("#hasil_prediksi").html(str_prediksi)
-}
+  }
 })
-
-console.log("Input Harga Rata-rata:", input_harga_rata);
-console.log("Input Rating:", input_rating);
-console.log("Input Kecamatan:", input_kecamatan);
-console.log("Input Kabupaten:", input_kabupaten);
-console.log("Input CR Angka:", input_cr_angka);
-
-  
